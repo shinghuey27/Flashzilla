80b39656322dea9ff09e361b3552c6ca0b1fd989
shinghuey27 <shinghuey27@hotmail.com>
2024-06-10 13:58:08 +0800

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Shing Huey on 10/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
